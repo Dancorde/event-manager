a29132411eaa3820d8217af54a4c423975446d24
Danilo Henrique Cordeiro <dancorde@gmail.com>
2019-07-08 13:54:21 -0300

    Added angular create route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { EventListComponent } from './events/event-list/event-list.component';
+import { EventCreateComponent } from './events/event-create/event-create.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: EventListComponent },
+  { path: 'create', component: EventCreateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
