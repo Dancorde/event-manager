75e6ce5bf70b1b6e65a1f422ff9a85f98cd60806
Danilo Henrique Cordeiro <dancorde@gmail.com>
2019-07-10 16:37:40 -0300

    Added bootstrap theme to calendar

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -5,20 +5,20 @@ import { CalendarService } from './calendar.service';
 import { Event } from '../events/event.model';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
+import bootstrapPlugin from '@fullcalendar/bootstrap';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html'
 })
 export class CalendarComponent implements OnInit {
-  calendarPlugins = [dayGridPlugin, timeGridPlugin];
+  calendarPlugins = [dayGridPlugin, timeGridPlugin, bootstrapPlugin];
   calendarHeader = {
     left: 'title',
-    center: 'dayGridMonth,timeGridWeek,timeGridDay',
+    center: 'dayGridMonth,timeGridDay',
     right: 'prev,next'
   };
   timeZone = 'America/Sao_Paulo';
-  locale = 'pt-br';
 
   events: Event[] = [];
   private eventsSub: Subscription;
