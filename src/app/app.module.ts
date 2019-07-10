618fc7646bfb12270a64c8048e0999e923bb8333
Danilo Henrique Cordeiro <dancorde@gmail.com>
2019-07-10 19:45:00 -0300

    Created separated modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 import { HeaderComponent } from './header/header.component';
 import { ErrorComponent } from './error/error.component';
 import { FullCalendarModule } from '@fullcalendar/angular';
-import { CalendarComponent } from './calendar/calendar.component';
 import { AngularMaterialModule } from './angular-material.module';
 import { EventsModule } from './events/event.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent,
-    SignupComponent,
     ErrorComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
     FullCalendarModule,
     AngularMaterialModule,
-    EventsModule
+    EventsModule,
+    AuthModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
